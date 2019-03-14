876bcdd3ad1454ec6181cad3d8869665bfbcd36c
Rémy Bourdoncle <bourdoncle.remy@gmail.com>
2019-03-14 19:14:00 +0100

    mise sous maven + ajout servlet

--- src/main/java/fr/rbo/Main.java
+++ src/main/java/fr/rbo/Main.java
@@ -12,14 +12,13 @@ public class Main {
         Customer customer = new Customer("Juste Leblanc", "19 rue Germain Pilon, Paris");
 
         Bill bill = new Bill(customer, new RelayDelivery(27));
-/*
+
         bill.addProduct(cafe, 1);
         bill.addProduct(tv, 1);
         bill.addProduct(fridge, 1);
+/*
+        bill.generate(new FileWriter("facture_leblanc"));
 */
-
- //       bill.generate(new FileWriter("facture_leblanc"));
-
         try {
             bill.generate(new Writer() {
                 @Override
